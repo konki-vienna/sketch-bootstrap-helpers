de504ecb5e8083a99e487fd0f255d965fb55d15c
Konstantin Demblin <Konstantin.Demblin@beeone.at>
2017-07-19 16:35:47 +0200

    Added some code
    Bad commit message, sorry.

--- Contents/Sketch/bootstrap_helpers.js
+++ Contents/Sketch/bootstrap_helpers.js
@@ -4,7 +4,7 @@ var selection;
 var selectedElement;
 var context;
 var bootstrapSize;
-var bootstrapColumnWidth;
+var gridColumnWidth;
 var artboard;
 var artboardName;
 var artboardWidth;
@@ -18,24 +18,25 @@ function onPlayground(context) {
 //Sets the selected element (idealy the bootstrap grid icon) to full grid width
 /*----------------------------------*/
 function onSetBootstrapGrid(context) {
-    checkArtboardSettings(context);
-    if (bootstrapSize == "xs") {
-        selectedElement.frame().width = artboard.frame().width();
-        selectedElement.frame().x = 0;
-    } else if (bootstrapSize == "sm") {
-        selectedElement.frame().width = 540;
-        selectedElement.frame().x = (artboard.frame().width() - selectedElement.frame().width()) * 0.5;
-    } else if (bootstrapSize == "md") {
-        selectedElement.frame().width = 720;
-        selectedElement.frame().x = (artboard.frame().width() - selectedElement.frame().width()) * 0.5;
-    } else if (bootstrapSize == "lg") {
-        selectedElement.frame().width = 960;
-        selectedElement.frame().x = (artboard.frame().width() - selectedElement.frame().width()) * 0.5;
-    } else if (bootstrapSize == "xl") {
-        selectedElement.frame().width = 1170;
-        selectedElement.frame().x = (artboard.frame().width() - selectedElement.frame().width()) * 0.5;
-    } else {
-        displayMessageToUser(context, "❌ Something went wrong. ❌");
+    if (checkArtboardSettings(context)) {
+        if (bootstrapSize == "xs") {
+            selectedElement.frame().width = artboard.frame().width();
+            selectedElement.frame().x = 0;
+        } else if (bootstrapSize == "sm") {
+            selectedElement.frame().width = 540;
+            selectedElement.frame().x = (artboard.frame().width() - selectedElement.frame().width()) * 0.5;
+        } else if (bootstrapSize == "md") {
+            selectedElement.frame().width = 720;
+            selectedElement.frame().x = (artboard.frame().width() - selectedElement.frame().width()) * 0.5;
+        } else if (bootstrapSize == "lg") {
+            selectedElement.frame().width = 960;
+            selectedElement.frame().x = (artboard.frame().width() - selectedElement.frame().width()) * 0.5;
+        } else if (bootstrapSize == "xl") {
+            selectedElement.frame().width = 1170;
+            selectedElement.frame().x = (artboard.frame().width() - selectedElement.frame().width()) * 0.5;
+        } else {
+            displayMessageToUser(context, "❌ Something went wrong. ❌");
+        }
     }
 
 }
@@ -49,6 +50,7 @@ function checkArtboardSettings(context) {
 
     if ([selection count] == 0) {
         displayMessageToUser(context, "❌ Please select an element. ❌");
+        return false;
     } else {
         context = context;
         doc = NSDocumentController.sharedDocumentController().currentDocument();
@@ -57,6 +59,7 @@ function checkArtboardSettings(context) {
 
         if (selectedElement.class() == "MSArtboardGroup") {
             displayMessageToUser(context, "❌ Please select an element and not an artboard. ❌");
+            return false;
         } else {
             bootstrapSize;
             artboard = selectedElement.parentArtboard();
@@ -64,9 +67,9 @@ function checkArtboardSettings(context) {
             artboardWidth = artboard.frame().width();
             pageName = artboard.parentPage().name();
             artboardIndex = artboard.parentPage().artboards().indexOf(artboard) + 1;
-            
+
             //log(artboardName + ", " + artboard.frame().width() + ", " + artboard.frame().height());
-            
+
             if (artboardWidth > 0 && artboardWidth <= 575) {
                 bootstrapSize = "xs";
             } else if (artboardWidth >= 576 && artboardWidth <= 767) {
@@ -80,9 +83,46 @@ function checkArtboardSettings(context) {
             } else {
                 bootstrapSize = "unknown";
             }
-            bootstrapColumnWidth = getColumnWidth();
-            displayMessageToUser(context, "✅ bootstrapSize: " + bootstrapSize + "; columnWidth: " + bootstrapColumnWidth + " ✅");
+            gridColumnWidth = getColumnWidth();
+            displayMessageToUser(context, "✅ bootstrapSize: " + bootstrapSize + "; columnWidth: " + gridColumnWidth + " ✅");
+
+            return true;
         }
+    }
+}
+
+function onIncreaseByOne(context) {
+    if (checkArtboardSettings(context)) {
+        var temp = getElementsCurrentColumnWidth();
+        displayMessageToUser(context, "✅ "+ temp +" ✅");
+    }
+}
+
+function getElementsCurrentColumnWidth() {
+    if (selectedElement.frame().width() <= gridColumnWidth) {
+        return 1;
+    } else if (selectedElement.frame().width() > gridColumnWidth && selectedElement.frame().width() <= (gridColumnWidth + 15) * 2) {
+        return 2;
+    } else if (selectedElement.frame().width() > gridColumnWidth && selectedElement.frame().width() <= (gridColumnWidth + 15) * 3) {
+        return 3;
+    } else if (selectedElement.frame().width() > gridColumnWidth && selectedElement.frame().width() <= (gridColumnWidth + 15) * 4) {
+        return 4;
+    } else if (selectedElement.frame().width() > gridColumnWidth && selectedElement.frame().width() <= (gridColumnWidth + 15) * 5) {
+        return 5;
+    } else if (selectedElement.frame().width() > gridColumnWidth && selectedElement.frame().width() <= (gridColumnWidth + 15) * 6) {
+        return 6;
+    } else if (selectedElement.frame().width() > gridColumnWidth && selectedElement.frame().width() <= (gridColumnWidth + 15) * 7) {
+        return 7;
+    } else if (selectedElement.frame().width() > gridColumnWidth && selectedElement.frame().width() <= (gridColumnWidth + 15) * 8) {
+        return 8;
+    } else if (selectedElement.frame().width() > gridColumnWidth && selectedElement.frame().width() <= (gridColumnWidth + 15) * 9) {
+        return 9;
+    } else if (selectedElement.frame().width() > gridColumnWidth && selectedElement.frame().width() <= (gridColumnWidth + 15) * 10) {
+        return 10;
+    } else if (selectedElement.frame().width() > gridColumnWidth && selectedElement.frame().width() <= (gridColumnWidth + 15) * 11) {
+        return 11;
+    } else if (selectedElement.frame().width() > (gridColumnWidth + 15) * 12) {
+        return 12;
     }
 }
 
