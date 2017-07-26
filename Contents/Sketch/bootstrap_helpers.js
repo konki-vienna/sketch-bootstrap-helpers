53aa274e4c1eff4a82fa241fe073439111f08f56
Konstantin Demblin <Konstantin.Demblin@beeone.at>
2017-07-26 08:39:00 +0200

    Rewrote plugin
    Rewrote several functions and removed bunch of global variables.

--- Contents/Sketch/bootstrap_helpers.js
+++ Contents/Sketch/bootstrap_helpers.js
@@ -1,125 +1,120 @@
 @import 'common.js'
 //var doc;
-var selection;
-var selectedElement;
-var selectedElement2;
-var context;
+//var context;
 var bootstrapSize;
 var gridTotalWidth;
 var gridColumnWidth;
 var gridGutter = 15;
-var artboard;
-var artboardName;
-var artboardWidth;
-var artboardIndex;
+var master;
+var slave;
 
 var reference_obj = new Object();
 
 function onPlayground(context) {
     //checkArtboardSettings(context);
-    //selectedElement.frame().width = 10;
 
-    onDefineReference(context);
+    if (onInitialize(context)) {
+        log("onPlayground successfully executed. :)")
+
+        //Sets the opacity of the sketch window to 50% (1 = 100%)
+        if (NSApplication.sharedApplication().mainWindow().alphaValue() == 0.5) {
+            NSApplication.sharedApplication().mainWindow().alphaValue = 1;
+        } else {
+            NSApplication.sharedApplication().mainWindow().alphaValue = 0.5;
+        }
+        displayMessageToUser(context, "✅ Alpha Value: " + NSApplication.sharedApplication().mainWindow().alphaValue() + " ✅");
+    };
 }
 
-function onDefineReference(context) {
-    selection = context.selection;
-    selectedElement = nil;
+function onInitialize(context) {
+    var selection = context.selection;
+
+    master = new Object();
+    slave = new Object();
 
     if ([selection count] == 0) {
-        displayMessageToUser(context, "❌ Please select an element. ❌");
+        displayMessageToUser(context, "❌ Please select one or two elements. ❌");
         return false;
     } else {
+        if ([selection count] == 1) {
+            //SINGLE OBJECT SELECTED
+            if ([selection objectAtIndex: 0].class() == "MSArtboardGroup") {
+                displayMessageToUser(context, "❌ Please select an element and not an artboard. ❌");
+                return false;
+            } else {
+                slave.element = [selection objectAtIndex: 0];
+                slave.name = slave.element.name();
 
-        selectedElement = [selection objectAtIndex: 0];
-        
-        reference_obj.name = selectedElement.name();
+                master.element = slave.element.parentArtboard();
+                master.name = master.element.name();
+                master.width = master.element.frame().width();
+
+                if (master.width > 0 && master.width <= 575) {
+                    bootstrapSize = "xs";
+                    gridTotalWidth = master.width;
+                } else if (master.width >= 576 && master.width <= 767) {
+                    bootstrapSize = "sm";
+                    gridTotalWidth = 540;
+                } else if (master.width >= 768 && master.width <= 991) {
+                    bootstrapSize = "md";
+                    gridTotalWidth = 720;
+                } else if (master.width >= 992 && master.width <= 1199) {
+                    bootstrapSize = "lg";
+                    gridTotalWidth = 960;
+                } else if (master.width >= 1200) {
+                    bootstrapSize = "xl";
+                    gridTotalWidth = 1170;
+                }
+                gridColumnWidth = getColumnWidth();
+
+                log(master.name + ", " + master.width + ", " + gridColumnWidth + ", " + bootstrapSize);
+
+                displayMessageToUser(context, "✅ One element selected: " + slave.name + " ✅");
+                return true;
+            }
+        } else if ([selection count] == 2) {
+            //TWO OBJECTS SELECTED
+            slave.element = [selection objectAtIndex: 0];
+            slave.name = slave.element.name();
+
+            master.element = [selection objectAtIndex: 1];
+            master.name = master.element.name();
+
+
+
+            displayMessageToUser(context, "✅ Two elements selected: " + slave.name + ", " + master.name + " ✅");
+            return true;
+
+        } else {
+            displayMessageToUser(context, "❌ Please select not more than two elements. ❌");
+            return false;
+        }
+
     }
-
-    displayMessageToUser(context, "✅ Reference: " + reference_obj.name + " ✅");
 }
 
 /*----------------------------------*/
 //Sets the selected element (idealy the bootstrap grid symbol) to full grid width
 /*----------------------------------*/
 function onSetBootstrapGrid(context) {
-    if (checkArtboardSettings(context)) {
+    if (onInitialize(context)) {
         if (bootstrapSize == "xs") {
-            selectedElement.frame().width = artboard.frame().width();
-            selectedElement.frame().x = 0;
+            slave.element.frame().width = master.element.frame().width();
+            slave.element.frame().x = 0;
         } else if (bootstrapSize == "sm") {
-            selectedElement.frame().width = 540;
-            selectedElement.frame().x = (artboard.frame().width() - selectedElement.frame().width()) * 0.5;
+            slave.element.frame().width = 540;
+            slave.element.frame().x = (master.element.frame().width() - slave.element.frame().width()) * 0.5;
         } else if (bootstrapSize == "md") {
-            selectedElement.frame().width = 720;
-            selectedElement.frame().x = (artboard.frame().width() - selectedElement.frame().width()) * 0.5;
+            slave.element.frame().width = 720;
+            slave.element.frame().x = (master.element.frame().width() - slave.element.frame().width()) * 0.5;
         } else if (bootstrapSize == "lg") {
-            selectedElement.frame().width = 960;
-            selectedElement.frame().x = (artboard.frame().width() - selectedElement.frame().width()) * 0.5;
+            slave.element.frame().width = 960;
+            slave.element.frame().x = (master.element.frame().width() - slave.element.frame().width()) * 0.5;
         } else if (bootstrapSize == "xl") {
-            selectedElement.frame().width = 1170;
-            selectedElement.frame().x = (artboard.frame().width() - selectedElement.frame().width()) * 0.5;
+            slave.element.frame().width = 1170;
+            slave.element.frame().x = (master.element.frame().width() - slave.element.frame().width()) * 0.5;
         } else {
             displayMessageToUser(context, "❌ Something went wrong. ❌");
-        }
-    }
-
-}
-
-/*----------------------------------*/
-//Initializes the plugin based on the selection
-/*----------------------------------*/
-function checkArtboardSettings(context) {
-    selection = context.selection;
-    selectedElement = nil;
-
-    if ([selection count] == 0) {
-        displayMessageToUser(context, "❌ Please select an element. ❌");
-        return false;
-    } else {
-        context = context;
-        //doc = NSDocumentController.sharedDocumentController().currentDocument();
-
-        selectedElement = [selection objectAtIndex: 0];
-        if ([selection count] > 1) {
-            selectedElement2 = [selection objectAtIndex: 1];
-
-            log(selectedElement.frame().width() + ", " + selectedElement2.frame().width());
-        }
-
-        if (selectedElement.class() == "MSArtboardGroup") {
-            displayMessageToUser(context, "❌ Please select an element and not an artboard. ❌");
-            return false;
-        } else {
-            bootstrapSize;
-            artboard = selectedElement.parentArtboard();
-            artboardName = artboard.name();
-            artboardWidth = artboard.frame().width();
-            pageName = artboard.parentPage().name();
-            artboardIndex = artboard.parentPage().artboards().indexOf(artboard) + 1;
-
-            //log(artboardName + ", " + artboard.frame().width() + ", " + artboard.frame().height());
-
-            if (artboardWidth > 0 && artboardWidth <= 575) {
-                bootstrapSize = "xs";
-                gridTotalWidth = artboardWidth;
-            } else if (artboardWidth >= 576 && artboardWidth <= 767) {
-                bootstrapSize = "sm";
-                gridTotalWidth = 540;
-            } else if (artboardWidth >= 768 && artboardWidth <= 991) {
-                bootstrapSize = "md";
-                gridTotalWidth = 720;
-            } else if (artboardWidth >= 992 && artboardWidth <= 1199) {
-                bootstrapSize = "lg";
-                gridTotalWidth = 960;
-            } else if (artboardWidth >= 1200) {
-                bootstrapSize = "xl";
-                gridTotalWidth = 1170;
-            }
-            gridColumnWidth = getColumnWidth();
-            displayMessageToUser(context, "✅ bootstrapSize: " + bootstrapSize + "; columnWidth: " + gridColumnWidth + " ✅");
-
-            return true;
         }
     }
 }
@@ -151,37 +146,36 @@ function findColumnWidth(shallBeIncreased, myElementWidth, myGridColumnWidth, my
 }
 
 function onDecreaseByOne(context) {
-    if (checkArtboardSettings(context)) {
+    if (onInitialize(context)) {
 
-        selectedElement.frame().width = findColumnWidth(false, selectedElement.frame().width(), gridColumnWidth, gridGutter)
+        slave.element.frame().width = findColumnWidth(false, slave.element.frame().width(), gridColumnWidth, gridGutter)
 
-        displayMessageToUser(context, "✅ " + selectedElement.frame().width() + " ✅");
+        displayMessageToUser(context, "✅ " + slave.element.frame().width() + " ✅");
     }
 }
 
 function onIncreaseByOne(context) {
+    if (onInitialize(context)) {
 
-    if (checkArtboardSettings(context)) {
+        slave.element.frame().width = findColumnWidth(true, slave.element.frame().width(), gridColumnWidth, gridGutter)
 
-        selectedElement.frame().width = findColumnWidth(true, selectedElement.frame().width(), gridColumnWidth, gridGutter)
-
-        displayMessageToUser(context, "✅ " + selectedElement.frame().width() + " ✅");
+        displayMessageToUser(context, "✅ " + slave.element.frame().width() + " ✅");
     }
 }
 
 function onMoveLeftByOne(context) {
-    if (checkArtboardSettings(context)) {
-        displayMessageToUser(context, "✅ onMoveLeftByOne " + selectedElement.frame().x() + " ✅");
+    if (onInitialize(context)) {
+        slave.element.frame().x = slave.element.frame().x() - (gridColumnWidth + gridGutter * 2);
 
-        selectedElement.frame().x = selectedElement.frame().x() - (gridColumnWidth + gridGutter * 2);
+        displayMessageToUser(context, "✅ onMoveLeftByOne " + slave.element.frame().x() + " ✅");
     }
 }
 
 function onMoveRightByOne(context) {
-    if (checkArtboardSettings(context)) {
-        displayMessageToUser(context, "✅ onMoveRightByOne " + selectedElement.frame().x() + " ✅");
+    if (onInitialize(context)) {
+        slave.element.frame().x = slave.element.frame().x() + (gridColumnWidth + gridGutter * 2);
 
-        selectedElement.frame().x = selectedElement.frame().x() + (gridColumnWidth + gridGutter * 2);
+        displayMessageToUser(context, "✅ onMoveRightByOne " + slave.element.frame().x() + " ✅");
     }
 }
 
