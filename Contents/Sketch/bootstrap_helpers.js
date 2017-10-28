7ed9a7215f9f6b9977457109b716b3b63c28bfb2
Konstantin Demblin <Konstantin.Demblin@gmail.com>
2017-10-28 13:28:30 +0200

    Stop in the middle of it to continue with another idea...

--- Contents/Sketch/bootstrap_helpers.js
+++ Contents/Sketch/bootstrap_helpers.js
@@ -8,6 +8,7 @@ var gridGutter = 15;
 var gridGroupName = "B00T$TRAP-Grid";
 var myFillColor = "#FF33CC59"; //100% — FF, 95% — F2, 90% — E6, 85% — D9, 80% — CC, 75% — BF, 70% — B3, 65% — A6, 60% — 99, 55% — 8C, 50% — 80, 45% — 73, 40% — 66, 35% — 59, 30% — 4D, 25% — 40, 20% — 33, 15% — 26, 10% — 1A, 5% — 0D, 0% — 00
 var master;
+var page;
 
 var myDictionary = NSThread.mainThread().threadDictionary();
 
@@ -45,9 +46,9 @@ function drawBootstrapGrid(context, myHasOuterGutter) {
     var selection = context.selection;
 
 
-    if ([selection count] == 0 || [selection count] >= 2) {
+    if ([selection count] == 0 || [selection count] > 2) {
         displayMessageToUser(context, "❌ Please select a single element. ❌");
-    } else {
+    } else if ([selection count] == 1){
         master = setMaster([selection objectAtIndex: 0]);
         if ([selection objectAtIndex: 0].class() == "MSArtboardGroup") {
             myHasOuterGutter = true; //in case an Artboard is selected, there shall always be a gutter
@@ -80,14 +81,40 @@ function drawBootstrapGrid(context, myHasOuterGutter) {
 
         }
         displayMessageToUser(context, "GridWidth: " + gridTotalWidth + "px, ColumnWidth: " + gridColumnWidth + "px, gutterWidth: " + gridGutter + "px, hasOuterGutter: " + myHasOuterGutter);
+    } else if ([selection count] == 2) {
+      if ([selection objectAtIndex: 0].class() == "MSShapeGroup" && [selection objectAtIndex: 1].class() == "MSShapeGroup") {
+        //GROUP SELECTION - as a workaround to beeing able to get width & height
+        //Add to layer group
+        var temp_layerGroup_array = new Array();
+        // Add created shape group to the current page.
+        temp_layerGroup_array.push([selection objectAtIndex: 0]);
+        temp_layerGroup_array.push([selection objectAtIndex: 1]);
 
+        groupLayers(temp_layerGroup_array, "t3mpGroup");
+
+        //selectContaining(context, "t3mpGroup");
+        /*var temp_array = findLayersNamed_inContainer_filterByType("t3mpGroup");
+
+        // Loop through filtered layers and select them
+        var loop = [temp_array objectEnumerator]
+        [[doc currentPage] deselectAllLayers];
+        while (layer = [loop nextObject]) {
+            //if (layer.name() == gridGroupName) {
+                //subSetOfLayers_array.push(layer);
+                [layer select: true byExtendingSelection: true];
+                log(layer.name());
+            //}
+        }*/
+        //TODO - 1. Unselect all selected layers; 2. Select the GroupLayer; 3. ...
+      } else {
+        displayMessageToUser(context, "❌ Please select two shapes ❌");
+        return;
+      }
     }
 }
 
 function drawGrid(myMaster, myMasterIsArtboard, myHasOuterGutter) {
     //CREATE GRID
-    //doc = NSDocumentController.sharedDocumentController().currentDocument();
-
     var rectShape;
     var tempRect;
 
@@ -373,3 +400,60 @@ function onToggleVisibilityOfBootstrapGrids(context) {
 /*-----------------------------------------------------------*/
 //TOGGLE VISIBILITY OF ALL BOOTSTRAP GRIDS ON THAT PAGE - END
 /*-----------------------------------------------------------*/
+
+/*-----------------------------------
+//HELPERS - START
+-----------------------------------*/
+//FIND LAYERS BY NAME
+//function findLayersNamed_inContainer_filterByType(layerName, container, layerType) {
+var findLayersNamed_inContainer_filterByType = function(layerName, container, layerType) {
+    var predicate = (typeof layerType === 'undefined' || layerType == nil) ? NSPredicate.predicateWithFormat("name == %@", layerName) : NSPredicate.predicateWithFormat("name == %@ && class == %@", layerName, layerType)
+    return findLayersMatchingPredicate_inContainer_filterByType(predicate, container)
+}
+
+var findLayersMatchingPredicate_inContainer_filterByType = function(predicate, container, layerType) {
+    var scope;
+    switch (layerType) {
+        case MSPage :
+            scope = doc.pages()
+            return scope.filteredArrayUsingPredicate(predicate)
+        break;
+
+        case MSArtboardGroup :
+            if(typeof container !== 'undefined' && container != nil) {
+                if (container.className == "MSPage") {
+                    scope = container.artboards()
+                    return scope.filteredArrayUsingPredicate(predicate)
+                }
+            } else {
+                // search all pages
+                var filteredArray = NSArray.array()
+                var loopPages = doc.pages().objectEnumerator(), page;
+                while (page = loopPages.nextObject()) {
+                    scope = page.artboards()
+                    filteredArray = filteredArray.arrayByAddingObjectsFromArray(scope.filteredArrayUsingPredicate(predicate))
+                }
+                return filteredArray
+            }
+        break;
+
+        default :
+            if(typeof container !== 'undefined' && container != nil) {
+                scope = container.children()
+                return scope.filteredArrayUsingPredicate(predicate)
+            } else {
+                // search all pages
+                var filteredArray = NSArray.array()
+                var loopPages = doc.pages().objectEnumerator(), page;
+                while (page = loopPages.nextObject()) {
+                    scope = page.children()
+                    filteredArray = filteredArray.arrayByAddingObjectsFromArray(scope.filteredArrayUsingPredicate(predicate))
+                }
+                return filteredArray
+            }
+    }
+    return NSArray.array() // Return an empty array if no matches were found
+}
+/*-----------------------------------
+//HELPERS - END
+-----------------------------------*/
